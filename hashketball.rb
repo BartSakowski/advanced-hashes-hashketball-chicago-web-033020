fc7c216a5cc0003ecd6aa7b3ab2c0396affd41e8
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-26 20:21:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -188,10 +188,8 @@ def player_numbers(team_name)
 end
 
 def player_stats(player_name)
-
 i = 0
   if i < player_array.length do
-    binding.pry
     if player_array[i][:player_name] == player_name
       return player[i]
     else
