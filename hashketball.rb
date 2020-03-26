deb4549d0dee0186ff48a27de618d65908dc7e52
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-26 19:24:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -159,7 +159,7 @@ def team_colors(team) #takes a team name and returns an ARRAY of that team color
   colors_array = []
   game_hash.each do |loc, team_data|
     team_data.each do |stuff, things|
-      if team_data|stuff| == team_data
+      if team_data|stuff| == team
         return team_data[:colors]
       end
       colors_array
